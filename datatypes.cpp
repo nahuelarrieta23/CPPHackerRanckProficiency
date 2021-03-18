476623a293b2b54079165a8c0820c2be2a15885d
Nahuel Arrieta Duben <40294376+nahuelarrieta23@users.noreply.github.com>
2021-03-18 13:45:09 +0100

    Create datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int _int;
+    long _long;
+    char _ch;
+    float _float;
+    double _double;
+    //Reading
+    scanf("%d %ld %c %f %lf", &_int, &_long , &_ch , &_float , &_double );
+    //Printing
+    printf("%d\n%ld\n%c\n%f\n%lf\n", _int, _long , _ch , _float , _double );
+    
+    return 0;
+}
