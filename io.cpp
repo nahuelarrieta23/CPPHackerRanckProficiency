9b9420e5c49615a54fd8fd643140d902732bbf43
Nahuel Arrieta Duben <40294376+nahuelarrieta23@users.noreply.github.com>
2021-03-18 13:43:25 +0100

    Create io.cpp

---
+++ io.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int sum;
+    cin>>a>>b>>c;
+    sum = a+b+c;
+    cout<<sum<<endl;
+    return 0;
+}
